d4abbf66ef3e2c96a1e96b38a284370959da822c
Paweł Ostrowski <pasza@pasza.org>
2022-12-04 14:55:14 +0100

    day-04: working solution

--- 2022/day-04/src/lib.rs
+++ 2022/day-04/src/lib.rs
@@ -1,39 +1,55 @@
+fn line_to_nums(line: &str) -> [u32;4] {
+    line
+        .split([',', '-'])
+        .map(|s| s.parse::<u32>().unwrap())
+        .collect::<Vec<u32>>()
+        .try_into()
+        .unwrap()
+}
+
+fn completely_overlapping(assignments: &[u32;4]) -> bool {
+    let [a, b, c, d] = assignments;
+    (a >= c && b <= d) || (c >= a && d <= b)
+}
+
 pub fn process_part1(input: &str) -> String {
-    input.into()
+    input.lines()
+        .map(line_to_nums)
+        .filter(completely_overlapping)
+        .count()
+        .to_string()
+}
+
+fn partially_overlapping(assignments: &[u32;4]) -> bool {
+    let [a, b, c, d] = assignments;
+    (b >= c && b <= d) || (d >= a && d <= b)
 }
 
 pub fn process_part2(input: &str) -> String {
-    input.into()
+    input.lines()
+        .map(line_to_nums)
+        .filter(partially_overlapping)
+        .count()
+        .to_string()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    const INPUT: &str = ".234.....  2-4
-.....678.  6-8
-
-.23......  2-3
-...45....  4-5
-
-....567..  5-7
-......789  7-9
-
-.2345678.  2-8
-..34567..  3-7
-
-.....6...  6-6
-...456...  4-6
-
-.23456...  2-6
-...45678.  4-8";
+    const INPUT: &str = "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8";
 
     #[test]
     fn part1_works() {
-        assert_eq!("works", process_part1(INPUT));
+        assert_eq!("2", process_part1(INPUT));
     }
 
     #[test]
     fn part2_works() {
-        assert_eq!("works", process_part2(INPUT));
+        assert_eq!("4", process_part2(INPUT));
     }
 }
