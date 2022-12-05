68bf570cb60254e75b1f859b112791b56d14b0f8
Paweł Ostrowski <pasza@pasza.org>
2022-12-05 14:07:20 +0100

    daytemplate: updated partN tests to be ignored initially

--- 2022/day-template/src/lib.rs
+++ 2022/day-template/src/lib.rs
@@ -12,11 +12,13 @@ mod tests {
     const INPUT: &str = "";
 
     #[test]
+    #[ignore = "not implemented"]
     fn part1_works() {
         assert_eq!("works", process_part1(INPUT));
     }
 
     #[test]
+    #[ignore = "not implemented"]
     fn part2_works() {
         assert_eq!("works", process_part2(INPUT));
     }
