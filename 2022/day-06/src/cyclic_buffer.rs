9fc7f7ee135f602db225e87bb278277430d29ec1
Paweł Ostrowski <pasza@pasza.org>
2022-12-06 11:12:52 +0100

    day-06: working solution + cyclic buffer impl

---
+++ 2022/day-06/src/cyclic_buffer.rs
@@ -0,0 +1,81 @@
+pub struct CyclicBuffer<T, const N: usize> {
+    buf: [T; N],
+    pos_zero: usize,
+}
+
+impl<T, const N: usize> CyclicBuffer<T, N> {
+    pub fn new(init_buf: [T; N]) -> Self {
+        let buf: [T; N] = init_buf;
+        Self {
+            buf,
+            pos_zero: 0,
+        }
+    }
+
+    pub fn push(&mut self, elem: T) {
+        let next_pos = (self.pos_zero + 1) % N;
+        self.buf[self.pos_zero] = elem;
+        self.pos_zero = next_pos;
+    }
+
+    pub fn get(&self, i: usize) -> &T {
+        let real_i = (self.pos_zero + i) % N;
+        &self.buf[real_i]
+    }
+    
+    pub fn get_mut(&mut self, i: usize) -> &mut T {
+        let real_i = (self.pos_zero + i) % N;
+        &mut self.buf[real_i]
+    }
+}
+
+impl<'a, T, const N: usize> IntoIterator for &'a CyclicBuffer<T, N> {
+    type Item = &'a T;
+
+    type IntoIter = CyclicBufferIter<'a, T, N>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        CyclicBufferIter {
+            buf: self,
+            pos: 0,
+        }
+    }
+}
+
+pub struct CyclicBufferIter<'a, T, const N: usize> {
+    buf: &'a CyclicBuffer<T, N>,
+    pos: usize,
+}
+
+impl<'a, T, const N: usize> Iterator for CyclicBufferIter<'a, T, N> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.pos >= N {
+            None
+        } else {
+            let cur_pos = self.pos; 
+            self.pos += 1;
+            Some(self.buf.get(cur_pos))
+        }
+    }
+}
+
+#[cfg(test)]
+#[test]
+fn cyclic_buffer_works() {
+    let init = [1, 3, 3, 4];
+    let mut buf = CyclicBuffer::<i32, 4>::new(init);
+    buf.push(5);
+    buf.push(6);
+    let v = buf.into_iter()
+        .copied()
+        .collect::<Vec<i32>>();
+    println!("{:?}", v);
+    // todo: why does it allow this push, shouldn't buf be consumed by to_iter()?
+    buf.push(7);
+    let v = buf.into_iter()
+        .copied()
+        .collect::<Vec<i32>>();
+    println!("{:?}", v);
+}
---
+++ 2022/day-06/src/lib.rs
@@ -0,0 +1,75 @@
+mod cyclic_buffer;
+
+use std::collections::HashSet;
+use crate::cyclic_buffer::CyclicBuffer;
+
+
+fn is_unique<const N: usize>(window: &CyclicBuffer<char, N>) -> bool {
+    let unique_signals = window.into_iter()
+        .copied()
+        .collect::<HashSet<char>>();
+     
+    unique_signals.len() == N
+}
+
+fn get_start_signal_position<const N: usize>(input: &str) -> usize {
+    let mut chars = input.chars();
+    let mut init: [char; N] = ['_'; N];
+
+    (0..N).for_each(|i| init[i] = chars.next().expect("input too short"));
+    let mut window = CyclicBuffer::<char, N>::new(init);
+    if is_unique(&window) {
+        return N;
+    }
+
+    for (pos, c) in chars.enumerate() {
+        window.push(c);
+        if is_unique(&window) {
+            return pos + N + 1;
+        };
+    }
+    0 
+}
+
+pub fn process_part1(input: &str) -> String {
+    let pos = get_start_signal_position::<4>(input);
+    pos.to_string()
+}
+
+pub fn process_part2(input: &str) -> String {
+    let pos = get_start_signal_position::<14>(input);
+    pos.to_string()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const TEST_DATA1: [(&str, u32);4] = [
+        ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
+        ("nppdvjthqldpwncqszvftbrmjlhg", 6),
+        ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10),
+        ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11),
+    ];
+
+    #[test]
+    fn part1_works() {
+        for (input, expected_output) in TEST_DATA1 {
+            assert_eq!(expected_output.to_string(), process_part1(input));
+        }
+    }
+
+    const TEST_DATA2: [(&str, u32);5] = [
+        ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19),
+        ("bvwbjplbgvbhsrlpgdmjqwftvncz", 23),
+        ("nppdvjthqldpwncqszvftbrmjlhg", 23),
+        ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29),
+        ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26),
+    ];
+    #[test]
+    fn part2_works() {
+        for (input, expected_output) in TEST_DATA2 {
+            assert_eq!(expected_output.to_string(), process_part2(input));
+        }
+    }
+}
