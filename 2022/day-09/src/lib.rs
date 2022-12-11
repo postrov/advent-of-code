05a07a37ac7215a9276055fe46922c012fb36ddf
Postrov <pasza@pasza.org>
2022-12-11 23:38:24 +0100

    day-09: working solution

--- 2022/day-09/src/lib.rs
+++ 2022/day-09/src/lib.rs
@@ -1,12 +1,18 @@
-use std::collections::BTreeSet;
+use std::{collections::BTreeSet, iter::repeat, cmp::Ordering};
 
 use nom::{IResult, multi::separated_list1, character::complete::{newline, char, one_of, self}, sequence::separated_pair};
 
-enum Move {
-    Left(i32),
-    Right(i32),
-    Up(i32),
-    Down(i32),
+#[derive(Clone)]
+enum Dir {
+    Left,
+    Right,
+    Up,
+    Down,
+}
+
+struct Move {
+    dir: Dir,
+    dist: u32,
 }
 
 type Pos = (i32, i32);
@@ -18,44 +24,111 @@ fn parse_moves(input: &str) -> IResult<&str, Vec<Move>> {
 }
 
 fn parse_move(input: &str) -> IResult<&str, Move> {
-    let (input, (dir, len)) = separated_pair(one_of("LRUD"), char(' '), complete::i32)(input)?;
+    let (input, (dir, dist)) = separated_pair(one_of("LRUD"), char(' '), complete::u32)(input)?;
 
-    let m = match dir {
-        'U' => Move::Up(len),
-        'D' => Move::Down(len),
-        'R' => Move::Right(len),
-        'L' => Move::Left(len),
-        _ => panic!("Bro, what a move!") // todo: handle error properly
+    let dir = match dir {
+        'U' => Dir::Up,
+        'D' => Dir::Down,
+        'R' => Dir::Right,
+        'L' => Dir::Left,
+        _ => unreachable!("Bro, what a move!") // todo: handle error properly
+    };
+    let m = Move {
+        dir,
+        dist,
     };
     Ok((input, m))
 }
 
-fn calculate_step(pos: Pos, m: Move) -> Pos {
-Match m {
-        Move::Up(len) => (pos.0, pos.1 + len),
-        Move::Down(len) => (pos.0, pos.1 - len),
-        Move::Left(len) => (pos.0 - len, pos.1),
-        Move::Right(len) => (pos.0 + len, pos.1),
+fn calculate_step(pos: Pos, dir: &Dir) -> Pos {
+    match dir {
+        Dir::Up => (pos.0, pos.1 + 1),
+        Dir::Down => (pos.0, pos.1 - 1),
+        Dir::Left => (pos.0 - 1, pos.1),
+        Dir::Right => (pos.0 + 1, pos.1),
     }
 }
 
 pub fn process_part1(input: &str) -> String {
-    let mut h = (0, 0);
-    let mut t = (0, 0);
-    let mut trail = BTreeSet::<Pos>::new();
+    struct State {
+        h: Pos, 
+        t: Pos,
+        trail: BTreeSet<Pos>,
+    }
 
-    let moves = parse_moves(input).unwrap();
-    input.into()
+    let state = State {
+        h: (0, 0),
+        t: (0, 0),
+        trail: BTreeSet::new(),
+    };
+
+    let (_input, moves) = parse_moves(input).unwrap();
+    moves.iter()
+        .flat_map(|Move {dir, dist}| repeat(dir).take(*dist as usize))
+        .fold(state, |mut state, dir| {
+            let new_h = calculate_step(state.h, dir);
+            let new_t = calculate_catch_up(new_h, state.t);
+            state.trail.insert(new_t);
+            state.h = new_h;
+            state.t = new_t;
+            state
+        }).trail
+        .len()
+        .to_string()
+}
+
+fn calculate_catch_up(h: Pos, t: Pos) -> Pos {
+    fn normalize(d: i32) -> i32 {
+        match d.cmp(&0) {
+            Ordering::Less => -1,
+            Ordering::Equal => 0,
+            Ordering::Greater => 1,
+        }
+    }
+    let dx = h.0 - t.0;
+    let dy = h.1 - t.1;
+    let adx = dx.abs(); 
+    let ady = dy.abs();
+
+    if adx >= 2 || ady >= 2 {
+        (t.0 + normalize(dx), t.1 + normalize(dy))
+    } else {
+        t
+    } 
 }
 
 pub fn process_part2(input: &str) -> String {
-    input.into()
+    const N: usize = 10;
+    struct State {
+        rope: [Pos; N],
+        trail: BTreeSet<Pos>,
+    }
+    let state = State {
+        rope: [(0, 0); N],
+        trail: BTreeSet::new(),
+    };
+    let (_input, moves) = parse_moves(input).unwrap();
+
+    moves.iter()
+        .flat_map(|Move {dir, dist}| repeat(dir).take(*dist as usize))
+        .fold(state, |mut state, dir| {
+            let mut new_rope = [(0, 0); N];
+            new_rope[0] = calculate_step(state.rope[0], dir);
+            (1..N).for_each(|i| {
+                new_rope[i] = calculate_catch_up(new_rope[i - 1], state.rope[i]);
+            });
+            state.trail.insert(new_rope[N - 1]);
+            state.rope = new_rope;
+            state
+        }).trail
+        .len()
+        .to_string()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    const INPUT: &str = "R 4
+    const INPUT1: &str = "R 4
 U 4
 L 3
 D 1
@@ -65,14 +138,20 @@ L 5
 R 2";
 
     #[test]
-    #[ignore = "not implemented"]
     fn part1_works() {
-        assert_eq!("13", process_part1(INPUT));
+        assert_eq!("13", process_part1(INPUT1));
     }
 
+const INPUT2: &str = "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20";
     #[test]
-    #[ignore = "not implemented"]
     fn part2_works() {
-        assert_eq!("works", process_part2(INPUT));
+        assert_eq!("36", process_part2(INPUT2));
     }
 }
