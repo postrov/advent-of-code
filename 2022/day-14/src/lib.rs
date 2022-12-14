9132058bee941d4053906bfa48c73ffb2324c534
Paweł Ostrowski <pasza@pasza.org>
2022-12-14 08:56:16 +0100

    day-14: initial scaffolding

---
+++ 2022/day-14/src/lib.rs
@@ -0,0 +1,26 @@
+pub fn process_part1(input: &str) -> String {
+    input.into()
+}
+
+pub fn process_part2(input: &str) -> String {
+    input.into()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const INPUT: &str = "498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9";
+
+    #[test]
+    #[ignore = "not implemented"]
+    fn part1_works() {
+        assert_eq!("24", process_part1(INPUT));
+    }
+
+    #[test]
+    #[ignore = "not implemented"]
+    fn part2_works() {
+        assert_eq!("works", process_part2(INPUT));
+    }
+}
