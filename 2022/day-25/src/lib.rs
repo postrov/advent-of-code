8c58e93486d213e9e45a8438c5a277d3eedc6747
Paweł Ostrowski <pasza@pasza.org>
2022-12-25 22:06:08 +0100

    day-25: cleanup

--- 2022/day-25/src/lib.rs
+++ 2022/day-25/src/lib.rs
@@ -1,39 +1,62 @@
+use std::{str::FromStr, iter::Sum, fmt::Display};
+
 use nom::{character::complete::{line_ending, one_of}, multi::{many1, separated_list1}, IResult, Parser};
 
-fn to_snafu(n: i64) -> String {
-    let mut remaining = n;
-    let mut digits = Vec::new();
-    while remaining > 0 {
-        let (digit, carry_over) = match remaining % 5 { 
-            0 => ('0', 0),
-            1 => ('1', 0),
-            2 => ('2', 0),
-            3 => ('=', 1),
-            4 => ('-', 1),
-            _ => unreachable!(),
-        };
-        digits.push(digit);
-        remaining = remaining / 5 + carry_over;
-    }
-    digits.iter().rev()
-        .collect()
+struct Snafu {
+    value: i64,
 }
 
-fn from_snafu(s: &str) -> Result<i64, &str> {
-    let mut result = 0;
-    let mut base = 1;
-    for digit in s.chars().rev() {
-        result += match digit {
-            '0' => 0,
-            '1' => 1,
-            '2' => 2,
-            '-' => -1,
-            '=' => -2,
-            _ => return Err("Invalid digit"),
-        } * base;
-        base *= 5;
+impl FromStr for Snafu {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut result = 0;
+        let mut base = 1;
+        for digit in s.chars().rev() {
+            result += match digit {
+                '0' => 0,
+                '1' => 1,
+                '2' => 2,
+                '-' => -1,
+                '=' => -2,
+                _ => return Err("Invalid digit".into()),
+            } * base;
+            base *= 5;
+        }
+        Ok(Snafu { value: result })
     }
-    Ok(result)
+}
+
+impl Sum for Snafu {
+    fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
+        Snafu {
+            value: iter
+                .map(|s| s.value)
+                .sum::<i64>(),
+        }
+    }
+}
+
+impl Display  for Snafu {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut remaining = self.value;
+        let mut digits = Vec::new();
+        while remaining > 0 {
+            let (digit, carry_over) = match remaining % 5 { 
+                0 => ('0', 0),
+                1 => ('1', 0),
+                2 => ('2', 0),
+                3 => ('=', 1),
+                4 => ('-', 1),
+                _ => unreachable!(),
+            };
+            digits.push(digit);
+            remaining = remaining / 5 + carry_over;
+        }
+        let str: String = digits.iter().rev()
+            .collect();
+        f.write_str(&str)
+    }
 }
 
 fn snafu_nums(input: &str) -> IResult<&str, Vec<String>> {
@@ -44,12 +67,10 @@ fn snafu_nums(input: &str) -> IResult<&str, Vec<String>> {
 pub fn process_part1(input: &str) -> String {
     let (_input, snafu_nums) = snafu_nums(input).unwrap();
 
-    let sum = snafu_nums.iter()
-        .map(|s| from_snafu(s).unwrap())
-        .sum::<i64>();
-        
-
-    to_snafu(sum)
+    snafu_nums.iter()
+        .map(|s| s.parse::<Snafu>().unwrap())
+        .sum::<Snafu>()
+        .to_string()
 }
 
 pub fn process_part2(input: &str) -> String {
@@ -58,6 +79,8 @@ pub fn process_part2(input: &str) -> String {
 
 #[cfg(test)]
 mod tests {
+    use rstest::rstest;
+
     use super::*;
     const INPUT: &str = "1=-0-2
 12111
@@ -73,37 +96,42 @@ mod tests {
 1=
 122";
 
-    #[test]
-    fn to_snafu_works() {
-        assert_eq!("1=1", to_snafu(16));
-        assert_eq!("1=2", to_snafu(17));
-        assert_eq!("1-=", to_snafu(18));
-        assert_eq!("1--", to_snafu(19));
-        assert_eq!("1121-1110-1=0", to_snafu(314159265));
-//                 1              1
-//         2              2
-//         3             1=
-//         4             1-
-//         5             10
-//         6             11
-//         7             12
-//         8             2=
-//         9             2-
-//        10             20
-//        15            1=0
-//        20            1-0
-//      2022         1=11-2
-//     12345        1-0---0
-// 314159265  1121-1110-1=0
+    #[rstest]
+    #[case("2", 2)] 
+    #[case("1=", 3)] 
+    #[case("1-", 4)] 
+    #[case("10", 5)] 
+    #[case("11", 6)] 
+    #[case("12", 7)] 
+    #[case("2=", 8)] 
+    #[case("2-", 9)] 
+    #[case("20", 10)] 
+    #[case("1=0", 15)] 
+    #[case("1-0", 20)] 
+    #[case("1=11-2", 2022)] 
+    #[case("1-0---0", 12345)] 
+    #[case("1121-1110-1=0", 314159265)] 
+    fn to_snafu_works(#[case] expected: &str, #[case] input: i64) {
+        assert_eq!(Snafu { value: input }.to_string(), expected);
     }
 
-    #[test]
-    fn from_snafu_works() {
-        assert_eq!(from_snafu("1=1").unwrap(), 16);
-        assert_eq!(from_snafu("1=2").unwrap(), 17);
-        assert_eq!(from_snafu("1-=").unwrap(), 18);
-        assert_eq!(from_snafu("1--").unwrap(), 19);
-        assert_eq!(from_snafu("1121-1110-1=0").unwrap(), 314159265);
+    #[rstest]
+    #[case("2", 2)] 
+    #[case("1=", 3)] 
+    #[case("1-", 4)] 
+    #[case("10", 5)] 
+    #[case("11", 6)] 
+    #[case("12", 7)] 
+    #[case("2=", 8)] 
+    #[case("2-", 9)] 
+    #[case("20", 10)] 
+    #[case("1=0", 15)] 
+    #[case("1-0", 20)] 
+    #[case("1=11-2", 2022)] 
+    #[case("1-0---0", 12345)] 
+    #[case("1121-1110-1=0", 314159265)] 
+    fn from_snafu_works(#[case] input: &str, #[case] expected: i64) {
+        assert_eq!(input.parse::<Snafu>().unwrap().value, expected);
     }
 
     #[test]
